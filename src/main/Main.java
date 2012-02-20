44b7a87cc79be090895b09d68aeaed0938f4b5e1
Maxim Manuylov <maxim.manuylov@gmail.com>
2012-02-20 11:21:39 +0100

    first commit

---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Maxim.Manuylov
+ * Date: 2/20/12
+ * Time: 11:07 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+}
