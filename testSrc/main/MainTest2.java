9d7ad8fe9fdd767a57c2b7a6674bdf5eebf4d3f2
Maxim Manuylov <maxim.manuylov@gmail.com>
2012-03-12 19:33:47 +0100

    tests

--- testSrc/main/MainTest2.java
+++ testSrc/main/MainTest2.java
@@ -2,6 +2,8 @@ package main;
 
 import org.testng.annotations.Test;
 
+import java.util.Random;
+
 @Test
 public class MainTest2 extends BaseTestCase {
     public void test_success() {
@@ -10,5 +12,10 @@ public class MainTest2 extends BaseTestCase {
 
     public void test_failure() {
         fail();
+    }
+
+    @Test(invocationCount = 10)
+    public void test_random() {
+        assertTrue(new Random().nextBoolean());
     }
 }
