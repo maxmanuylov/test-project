4f64ecb9a342c34a503704054000d7f10200d87e
Maxim Manuylov <maxim.manuylov@gmail.com>
2012-07-11 19:06:44 +0400

    2

--- testSrc/main/MainTest.java
+++ testSrc/main/MainTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 @Test
 public class MainTest extends BaseTestCase {
     public void test_success() {
-        assertEquals(1, 2);
+        assertEquals(1, 1);
     }
 
     public void test_failure() {
