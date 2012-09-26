f796ba618dd10e5a5c37cf1a4d7e67f37c0ae001
Maxim Manuylov <maxim.manuylov@gmail.com>
2012-09-26 20:59:53 +0200

    commit

---
+++ remote-debug-test/src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Maxim.Manuylov
+ * Date: 26.09.12
+ * Time: 20:57
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
